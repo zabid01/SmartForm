88624531abe235937eb2c6eab63843a8932c80a7
zabid01 <zabid.antor@gmail.com>
2018-02-12 09:27:32 +0600

    smartForm classwork

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,53 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+  name:'',
+  email:'',
+  html: {Likes:[], disLikes:[]},
+  css: {Likes:[], disLikes:[]},
+  js: {Likes:[], disLikes:[]},
+  strength: {css:'',html:'',js:'', },
+  currentQuestion: '#welcome'
+};
+
+$('#start').click(function(){
+  //console.log("start");
+  $('#welcome').hide();
+  $('#q1').show();
+});
+
+// $('#name').change(function(event){
+//   console.log($('#name').val());
+//   // $('#welcome').hide();
+//   // $('#q1').show();
+// });
+
+
+$('#q1next').click(function(){
+  //console.log("start");
+  if($('#name').val() && $('#name').val()){
+    userData.name= $('#name').val();
+    userData.email= $('#email').val();
+    //email varification
+    $('#q2').show();
+    $('#q1').hide();
+  }
+  else {
+    alert('Enter Name & Email');
+  }
+ });
+
+ $('#bhtml').click(function(){
+   $('#q2a').show();
+   $('#q2').hide();
+  });
+
+ $('#bcss').click(function(){
+   $('#q2b').show();
+   $('#q2').hide();
+  });
+
+ $('#bjava').click(function(){
+   $('#q2c').show();
+   $('#q2').hide();
+  });
